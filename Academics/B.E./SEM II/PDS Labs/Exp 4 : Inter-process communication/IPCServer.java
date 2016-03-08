d53524e11760d6d93389c4c2af36820d20e99111
mohan08p <mohanpawary1990@gmail.com>
2016-03-08 16:30:33 +0000

    Exp 4 : Inter-process Communication

---
+++ Academics/B.E./SEM II/PDS Labs/Exp 4 : Inter-process communication/IPCServer.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.*;
+
+public class IPServer
+{
+     public static void main(String[] args)
+     {
+          System.out.println("\n **** INTERPROCESS COMMUNICATION ****\n");
+          System.out.println("\n *** SERVER PROCCESS STARTED ***\n");
+          System.out.println("\n SERVER IS READY AND WAITING TO RECEIVE DATA FROM CLIENT PROCESS ON PORT "+1200);
+          try
+          {
+               ServerSocket ss = new ServerSocket(1200); // on port number 1200
+               Socket clientSocket = ss.accept();
+               System.out.println("\n Client is connected with IP address"+clientSocket.getInetAddress() + " nd port number "+clientSocket.getPort());
+               dataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+               DataInputStream dis = new dataInputStream(clientSocket.getInputStream());
+               int a = dis.readInt();
+               System.out.println("\n SERVER RECEIVED");
+               System.out.println("\n Number 1 -> "+a);
+               int b = dis.readInt();
+               System.out.println("\n Number 2 -> "+b);
+               int c = a + b;
+               dos.writeInt(c);
+               System.out.println("\n SERVER PROCESS HAS EXECUTED REQUESTED PROCESS AND SENT RESULT "+c+" TO THE CLIENT \n");
+               clientSocket.close();
+               System.out.println("\n SERVER PROCESS EXITING....");
+               ss.close();
+          }
+          catch(Exception e)
+          {
+               System.out.println("Exception : "+e);
+          }
+     }
+}
---
+++ Academics/B.E./SEM II/PDS Labs/Exp 4 : Inter-process communication/README.md
@@ -0,0 +1,13 @@
+Inter-process Communication
+
+Steps to perform Interprocess Communication in using Client and Server program :
+
+Step 1 : Write server program IPCServer.java and compile it.
+
+Step 2 : Write client program IPCClient.java and compile it.
+
+Step 3 : Run Server Program.
+
+Step 4 : Run Client Program.
+
+Step 5 : Get the output at server and client site, respectively.
